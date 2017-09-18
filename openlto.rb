e35e0efab1e82b7e16d1b11723f91ac350c09cfb
Reto Kromer <retokromer@users.noreply.github.com>
2017-09-18 13:51:08 +0200

    add openlto placeholder

---
+++ openlto.rb
@@ -0,0 +1,16 @@
+class Openlto < Formula
+  desc "Bash scripts to manage LTO cartridges with LTFS"
+  homepage "https://avpres.net/openLTO/"
+  url "https://github.com/AVpres/openLTO/archive/openLTO-2017-09-18.tar.gz"
+  version "2017-09-18"
+  sha256 "9a76f3ceb40fc18093135bba067666a60f86cde3efecd22ddf3056768a572b51"
+
+  bottle :unneeded
+
+  conflicts_with "ltopers",
+    :because => "both install some scripts having the same name"
+
+  def install
+    man1.install "openlto.1"
+  end
+end
