74c96f49864611d7de291049a6a0b1f519824bd0
Reto Kromer <retokromer@users.noreply.github.com>
2017-09-19 10:36:56 +0200

    fix pre-alpha

--- openltfs.rb
+++ openltfs.rb
@@ -4,10 +4,12 @@ class Openltfs < Formula
   url "https://github.com/AVpres/openLTFS/archive/2017-09-19.tar.gz"
   version "2017-09-19"
   sha256 "657231f0959447c7731682016f051b60a076919c6b79cfebed63b3d746bb3d65"
+  revision 1
 
   bottle :unneeded
 
   def install
+    bin.install "openltfs"
     man1.install "openltfs.1"
   end
 end
