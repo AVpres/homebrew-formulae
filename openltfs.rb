6f32af9cdc05c1a3a1a0e953259d5014bc5b228e
Reto Kromer <retokromer@users.noreply.github.com>
2017-09-20 07:53:12 +0200

    pre-alpha 2017-09-20

--- openltfs.rb
+++ openltfs.rb
@@ -1,10 +1,9 @@
 class Openltfs < Formula
   desc "An open and neutral LTFS, without the vendors' extensions"
   homepage "https://avpres.net/openLTFS/"
-  url "https://github.com/AVpres/openLTFS/archive/2017-09-19.tar.gz"
-  version "2017-09-19"
-  sha256 "657231f0959447c7731682016f051b60a076919c6b79cfebed63b3d746bb3d65"
-  revision 1
+  url "https://github.com/AVpres/openLTFS/archive/2017-09-20.tar.gz"
+  version "2017-09-20"
+  sha256 "773d089fc24da2ce25a6b091d6fa27f5af7e1a24c660fd5ede69616beef33df9"
 
   bottle :unneeded
 
