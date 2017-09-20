ebf05f8058c9bc6e27259c1f8b67032817154f22
Reto Kromer <retokromer@users.noreply.github.com>
2017-09-20 07:53:58 +0200

    pre-alpha 2017-09-20

--- openlto.rb
+++ openlto.rb
@@ -1,10 +1,9 @@
 class Openlto < Formula
   desc "Bash scripts to manage LTO cartridges with LTFS"
   homepage "https://avpres.net/openLTO/"
-  url "https://github.com/AVpres/openLTO/archive/2017-09-19.tar.gz"
-  version "2017-09-19"
-  sha256 "c18a7934bb00ac6c094ee67898ad5800221fac9287a76f09d8962d4eae5425f5"
-  revision 1
+  url "https://github.com/AVpres/openLTO/archive/2017-09-20.tar.gz"
+  version "2017-09-20"
+  sha256 "125192bf12876a653db12c5cc5ff13cc4049af794eb4387ac1d654542fe81627"
 
   bottle :unneeded
 
