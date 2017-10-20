067c10e516168279f5e33e7fa6e0ae2b8d1a7bca
Reto Kromer <retokromer@users.noreply.github.com>
2017-10-20 11:32:31 +0200

    tap is needed

--- openlto.rb
+++ openlto.rb
@@ -4,10 +4,11 @@ class Openlto < Formula
   url "https://github.com/AVpres/openLTO/archive/2017-09-20.tar.gz"
   version "2017-09-20"
   sha256 "125192bf12876a653db12c5cc5ff13cc4049af794eb4387ac1d654542fe81627"
+  revision 1
 
   bottle :unneeded
 
-  conflicts_with "ltopers",
+  conflicts_with "amiaopensource/amiaos/ltopers",
     :because => "both install some scripts having the same name"
 
   def install
