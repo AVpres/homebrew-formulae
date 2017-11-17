0017b622077b73f4a109369fa37db3a1c449360b
Reto Kromer <retokromer@users.noreply.github.com>
2017-11-17 15:07:10 +0100

    fix version

--- openlto.rb
+++ openlto.rb
@@ -1,8 +1,8 @@
 class Openlto < Formula
   desc "Bash scripts to manage LTO cartridges with LTFS"
   homepage "https://avpres.net/openLTO/"
-  url "https://github.com/AVpres/openLTO/archive/2017-09-17.tar.gz"
-  version "2017-09-17"
+  url "https://github.com/AVpres/openLTO/archive/2017-11-17.tar.gz"
+  version "2017-11-17"
   sha256 "1ace8a3f9771c535c831746102672c2ebd61b35b5d3276d73ab3e655a0925b27"
 
   bottle :unneeded
