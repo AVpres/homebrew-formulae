3aae2a5372516f6ce490e044f309443bd0bb4c0c
Reto Kromer <retokromer@users.noreply.github.com>
2017-12-12 18:26:19 +0100

    openYCoCg 2017-12-12 (pre-alpha)

--- openycocg.rb
+++ openycocg.rb
@@ -1,9 +1,9 @@
 class Openycocg < Formula
   desc "Video codec, built on YCoCg colour space, for restoration purposes"
   homepage "https://avpres.net/openYCoCg/"
-  url "https://github.com/AVpres/openYCoCg/archive/2017-11-17.tar.gz"
-  version "2017-11-17"
-  sha256 "33137ce3085e9717c4ad3618b9471d9ccc149260647c04a24b65f27ea5319c9a"
+  url "https://github.com/AVpres/openYCoCg/archive/2017-12-12.tar.gz"
+  version "2017-12-12"
+  sha256 "a72bdb35f23d68ef620b1b9997466fcdca88d3657805586416a58588628fd779"
 
   bottle :unneeded
 
