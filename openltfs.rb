8e0423e4cd277a8520a54d7962b247c649358d3f
Reto Kromer <lists@reto.ch>
2018-07-07 15:46:11 +0200

    openLTFS release 2018-07-07

--- openltfs.rb
+++ openltfs.rb
@@ -1,9 +1,9 @@
 class Openltfs < Formula
   desc "CLI for libltfs library to handle LTFS openly and neutrally"
   homepage "https://avpres.net/openLTFS/"
-  url "https://avpres.net/openLTFS/releases/2018-06-30.tar.gz"
-  version "2018-06-30"
-  sha256 "5899b0a45ea00b4b639040b4a8b40c18dd5ea0ef1474df8cd74face326ed5380"
+  url "https://avpres.net/openLTFS/releases/2018-07-07.tar.gz"
+  version "2018-07-07"
+  sha256 "4714788c7cf89d6a272bb27e319890b593552c5e5f26312a8c59b75e3349ca2f"
 
   bottle :unneeded
 
