3ac3b5503e1dac620f7e0d6312d5ec49f27036e9
Reto Kromer <lists@reto.ch>
2018-08-10 16:49:36 +0200

    version 2018-08-11

--- openycocg.rb
+++ openycocg.rb
@@ -1,9 +1,9 @@
 class Openycocg < Formula
   desc "CLI for 'libycocg' to encode, decode, play and analyse YCoCg video"
   homepage "https://avpres.net/openYCoCg/"
-  url "https://avpres.net/openYCoCg/releases/2018-08-04.tar.gz"
-  version "2018-08-04"
-  sha256 "1f5d1c35fd707dc8ae78fae3da49789f3e8027b78951b6eca43196047234fcc8"
+  url "https://avpres.net/openYCoCg/releases/2018-08-11.tar.gz"
+  version "2018-08-11"
+  sha256 "30139a1243892ed764ff2142b0af67b1b3fc16c7f72753f8f800673df8faf67e"
 
   bottle :unneeded
 
