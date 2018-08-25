c97ff9747b26ddaab2d86e3efb687b4ad16ae606
Reto Kromer <lists@reto.ch>
2018-08-25 16:49:55 +0200

    release 2018-08-26
    - fix bug

--- openycocg.rb
+++ openycocg.rb
@@ -1,9 +1,9 @@
 class Openycocg < Formula
   desc "CLI for 'libycocg' to encode, decode, play and analyse YCoCg video"
   homepage "https://avpres.net/openYCoCg/"
-  url "https://avpres.net/openYCoCg/releases/2018-08-25.tar.gz"
-  version "2018-08-25"
-  sha256 "a75d32674e48324f047d7b81dc2ee4cd2918ee9bd879d2c63c0995f013823602"
+  url "https://avpres.net/openYCoCg/releases/2018-08-26.tar.gz"
+  version "2018-08-26"
+  sha256 "0f6192733fc50ad388e2a5e67abcca07d86823c3834656dfeafff3295f06911f"
 
   bottle :unneeded
 
