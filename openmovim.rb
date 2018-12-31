048b21a9bc4a87ff5fa98bbfec3993611a33e0a2
Reto Kromer <lists@reto.ch>
2018-12-31 10:52:43 +0100

    openmovim 2019-01-01

--- openmovim.rb
+++ openmovim.rb
@@ -1,9 +1,9 @@
 class Openmovim < Formula
   desc "CLI for 'libmovim' to encode, decode, play and analyse moving images"
   homepage "https://avpres.net/openMovIm/"
-  url "https://avpres.net/openMovIm/releases/2018-12-24.tar.gz"
-  version "2018-12-24"
-  sha256 "a2b98da16ccba474a7280ada786b56da2b8f24892949d44d8ae2a9d1df8c5d11"
+  url "https://avpres.net/openMovIm/releases/2019-01-01.tar.gz"
+  version "2019-01-01"
+  sha256 "71d40fe20fcaf25a87d1eeee0a395003c90e60916df8d8c2d8941210e68b42b1"
 
   bottle :unneeded
 
@@ -12,8 +12,8 @@ class Openmovim < Formula
       opoo "The 'libmovim' library cannot be installed. Please login."
     else
       patch do
-        url "https://avpres.net/patch/openmovim_2018-12-24.diff"
-        sha256 "61574906abe50dc41bc3b589da9bad97293f728d9120ab704552e8305bd907b9"
+        url "https://avpres.net/patch/openmovim_2019-01-01.diff"
+        sha256 "aaaed814cbb4be8d875ed5b3cd211b7d45f129b29fe9071c762670d701b63a03"
       end
       bin.install "libmovim"
     end
