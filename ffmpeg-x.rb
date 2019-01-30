fa1316ab39fe841c368dd1df013b2c1696ccb086
Reto Kromer <lists@reto.ch>
2019-01-30 08:35:41 +0100

    add FFmpeg 4 AV archivists

---
+++ ffmpeg-x.rb
@@ -0,0 +1,85 @@
+class FfmpegX < Formula
+  desc "FFmpeg for audio-visual archivists"
+  homepage "https://ffmpeg.org/"
+  url "https://ffmpeg.org/releases/ffmpeg-4.1.tar.xz"
+  sha256 "a38ec4d026efb58506a99ad5cd23d5a9793b4bf415f2c4c2e9c1bb444acd1994"
+
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "frei0r"
+  depends_on "lame"
+  depends_on "libass"
+  depends_on "libvorbis"
+  depends_on "libvpx"
+  depends_on "opencore-amr"
+  depends_on "openjpeg" ####
+  depends_on "opus"
+  depends_on "rtmpdump"
+  depends_on "rubberband" ####
+  depends_on "sdl2"
+  depends_on "snappy"
+  depends_on "speex"
+  depends_on "tesseract" ####
+  depends_on "theora"
+  depends_on "x264"
+  depends_on "x265"
+  depends_on "xvid"
+  depends_on "xz"
+
+  conflicts_with "ffmpeg",
+    :because => "both install the same resource"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --enable-shared
+      --enable-pthreads
+      --enable-version3
+      --enable-hardcoded-tables
+      --enable-avresample
+      --cc=#{ENV.cc}
+      --host-cflags=#{ENV.cflags}
+      --host-ldflags=#{ENV.ldflags}
+      --enable-ffplay
+      --enable-gpl
+      --enable-libmp3lame
+      --enable-libopus
+      --enable-libsnappy
+      --enable-libtheora
+      --enable-libvorbis
+      --enable-libvpx
+      --enable-libx264
+      --enable-libx265
+      --enable-libxvid
+      --enable-lzma
+      --enable-libfontconfig
+      --enable-libfreetype
+      --enable-frei0r
+      --enable-libass
+      --enable-libopencore-amrnb
+      --enable-libopencore-amrwb
+      --enable-librtmp
+      --enable-libspeex
+      --enable-videotoolbox
+      --enable-librubberband
+      --enable-libtesseract
+      --enable-libopenjpeg
+      --disable-decoder=jpeg2000
+      --extra-cflags=" + `pkg-config --cflags libopenjp2`.chomp
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+
+    # Build and install additional FFmpeg tools
+    system "make", "alltools"
+    bin.install Dir["tools/*"].select { |f| File.executable? f }
+  end
+
+  test do
+    # Create an example mp4 file
+    mp4out = testpath/"video.mp4"
+    system bin/"ffmpeg", "-filter_complex", "testsrc=rate=1:duration=1", mp4out
+    assert_predicate mp4out, :exist?
+  end
+end
