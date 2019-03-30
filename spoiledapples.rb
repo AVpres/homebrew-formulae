9ee9ed90e3582f1cef32d0f20c518b3d0b9f1283
Reto Kromer <lists@reto.ch>
2019-03-30 18:45:27 +0100

    fix bug

--- spoiledapples.rb
+++ spoiledapples.rb
@@ -3,7 +3,7 @@ class Spoiledapples < Formula
   homepage "https://avpres.net/Spoiled_Apples/"
   url "https://avpres.net/Spoiled_Apples/releases/2019-03-30.tar.gz"
   version "2019-03-30"
-  sha256 "25deeaea36a175b46b27735025178bd6814436dfd5c9f8f99938c574d1c79e34"
+  sha256 "ed1232bfe5ab420fb4cf29dc48f0c9da8b11e8059eddb889f7866a17fe1dc9d4"
 
   bottle :unneeded
 
@@ -13,7 +13,7 @@ class Spoiledapples < Formula
     else
       patch do
         url "https://avpres.net/patch/spoiledapples_2019-03-30.diff"
-        sha256 "3c7b05714461a2c5acd26d587de02d3cdc1a54c649889328aa27b6d8576f845d"
+        sha256 "f34dedec2e50b1cecd420fec07994a12ae8cf13fe3601206814bed9bfcf0ef0f"
       end
       args = %W[
         --prefix=#{prefix}
