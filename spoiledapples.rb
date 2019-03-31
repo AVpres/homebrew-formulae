498437e7f85c40b66c673fb03459cc0180b06255
Reto Kromer <lists@reto.ch>
2019-03-31 10:47:56 +0200

    2019-03-31 release
    - EPROM option improved

--- spoiledapples.rb
+++ spoiledapples.rb
@@ -1,9 +1,9 @@
 class Spoiledapples < Formula
   desc "Emulation of 6502, 680x0 and PowerPC-based Apple computers"
   homepage "https://avpres.net/Spoiled_Apples/"
-  url "https://avpres.net/Spoiled_Apples/releases/2019-03-30.tar.gz"
-  version "2019-03-30"
-  sha256 "2cfe1b4f788ac4c15a1cbc502f6c7031d74dd073110fdc82a5c691a5ada9952e"
+  url "https://avpres.net/Spoiled_Apples/releases/2019-03-31.tar.gz"
+  version "2019-03-31"
+  sha256 "6cfdc3b47cf3357c58d7f4856aa6e18b4b45b73bf77af3d5e2a2b9c7855ce998"
 
   bottle :unneeded
 
@@ -12,8 +12,8 @@ class Spoiledapples < Formula
       opoo "Sorry, the libraries cannot be installed. Please login."
     else
       patch do
-        url "https://avpres.net/patch/spoiledapples_2019-03-30.diff"
-        sha256 "6c38b24f161c594b4364704e93e5b6d44edfc67183a065ba679060f267e43f61"
+        url "https://avpres.net/patch/spoiledapples_2019-03-31.diff"
+        sha256 "71289915f934782bbfc274940d85b992916057de003f3ac47fdee91a15cad00a"
       end
       args = %W[
         --prefix=#{prefix}
