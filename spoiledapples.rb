0f9d5794fa00c21d2a47119a37fc35c8e1d4c0de
Reto Kromer <lists@reto.ch>
2019-05-03 21:06:02 +0200

    spoiledapples 2019-05-04

--- spoiledapples.rb
+++ spoiledapples.rb
@@ -1,9 +1,9 @@
 class Spoiledapples < Formula
   desc "Emulation of 6502, 680x0 and PowerPC-based Apple computers"
   homepage "https://avpres.net/Spoiled_Apples/"
-  url "https://avpres.net/Spoiled_Apples/releases/2019-04-06.tar.gz"
-  version "2019-04-06"
-  sha256 "5257341075142a9f2940a52564ad9dd2a177a6fcc514af296f3355926a9aaa5d"
+  url "https://avpres.net/Spoiled_Apples/releases/2019-05-04.tar.gz"
+  version "2019-05-04"
+  sha256 "c4c5261e55b01a3be749c9710387b9869d066c92fb160486e2986e0a98629866"
 
   bottle :unneeded
 
@@ -12,8 +12,8 @@ class Spoiledapples < Formula
       opoo "Sorry, the libraries cannot be installed. Please login."
     else
       patch do
-        url "https://avpres.net/patch/spoiledapples_2019-04-06.diff"
-        sha256 "225fc3872bf0f67f67ff20dd1fdf99b388163f54b568d66e31be42ab0bfb4200"
+        url "https://avpres.net/patch/spoiledapples_2019-05-04.diff"
+        sha256 "d9047927880deeba1ae33f999be2a056507291b9494a2da37451ec60d74b4d7d"
       end
       args = %W[
         --prefix=#{prefix}
