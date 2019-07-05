19ef2ae53612b71fae9e09ca02d4c3c93fb9aac3
Reto Kromer <lists@reto.ch>
2019-07-05 20:09:17 +0200

    update CineForm

--- cineform.rb
+++ cineform.rb
@@ -1,21 +1,21 @@
 class Cineform < Formula
   desc "The CineForm codec"
   homepage "https://gopro.github.io/cineform-sdk/"
-#  url "https://github.com/gopro/cineform-sdk/archive/v10.0.1.tar.gz"
-#  sha256 "89d170267a52cb7a0bf94d7ce18ac36d0b4d9f42d795aa7bf49e701b5259a28b"
+  url "https://github.com/gopro/cineform-sdk/archive/v10.0.1.tar.gz"
+  sha256 "89d170267a52cb7a0bf94d7ce18ac36d0b4d9f42d795aa7bf49e701b5259a28b"
   head "https://github.com/gopro/cineform-sdk.git"
 
   bottle :unneeded
 
-  depends_on "autoconf"
-  depends_on "automake"
-  depends_on "cmake"
-  depends_on "libtool"
-  depends_on "pkg-config"
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "cmake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
 
   def install
     system "cmake", "."
     system "make"
-    system "make", "install"
+    bin.install "libCFHDCodec.a"
   end
 end
