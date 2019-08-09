47a9acb1dc06b49420305e8b8b0d98ac84225948
Reto Kromer <lists@reto.ch>
2019-08-09 11:58:30 +0200

    release 2019-08-10

--- spoiledapples.rb
+++ spoiledapples.rb
@@ -1,8 +1,8 @@
 class Spoiledapples < Formula
   desc "Emulation of 6502, 680x0 and PowerPC-based Apple computers"
   homepage "https://avpres.net/Spoiled_Apples/"
-  url "https://avpres.net/releases/spoiledapples-2019-08-03.tar.gz"
-  sha256 "448fa7db6dbc18b908c47d26f60dfa16ec3a4f122acd582334c355e81fb1955b"
+  url "https://avpres.net/releases/spoiledapples-2019-08-10.tar.gz"
+  sha256 "fa9d51d2d6287d195889bbd75bf22df18ead702bd2c2bbbded6b2f4a8ce7ce84"
 
   bottle :unneeded
 
@@ -14,8 +14,8 @@ class Spoiledapples < Formula
         opoo "Please login for full installation."
       else
         patch do
-          url "https://avpres.net/patch/spoiledapples-2019-08-03.diff"
-          sha256 "478a1dfae1b5078a34d8a279c8fa724e9f5658be6d8cca19e1f3f5fc171e1f35"
+          url "https://avpres.net/patch/spoiledapples-2019-08-10.diff"
+          sha256 "bbb0b1a7fb9d32b54a2bd8054411a7f3d8347401f7bb87a15be236e4c53e5419"
         end
 
         args << "--enable-debugger" if build.with? "debugger"
