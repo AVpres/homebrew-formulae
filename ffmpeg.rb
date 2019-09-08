db9ae47cdab0916d26c1c8958817b1cdb6bf038c
Reto Kromer <lists@reto.ch>
2019-09-08 13:44:23 +0200

    move Python scripts out of bin

--- ffmpeg.rb
+++ ffmpeg.rb
@@ -3,6 +3,7 @@ class Ffmpeg < Formula
   homepage "https://ffmpeg.org/"
   url "https://ffmpeg.org/releases/ffmpeg-4.2.1.tar.xz"
   sha256 "cec7c87e9b60d174509e263ac4011b522385fd0775292e1670ecc1180c9bb6d4"
+  revision 1
   head "https://github.com/FFmpeg/FFmpeg.git"
 
   bottle :unneeded
@@ -177,6 +178,7 @@ class Ffmpeg < Formula
     # Build and install additional FFmpeg tools
     system "make", "alltools"
     bin.install Dir["tools/*"].select { |f| File.executable? f }
+    mv bin/"python", pkgshare/"python", :force => true
   end
 
   def caveats; <<~EOS
