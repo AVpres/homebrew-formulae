c24cfcde1106901054b15788dd5a5d4cd1bebcdd
Reto Kromer <lists@reto.ch>
2019-09-27 21:48:31 +0200

    remove `mpv` dependency

--- vrecord-x.rb
+++ vrecord-x.rb
@@ -4,6 +4,7 @@ class VrecordX < Formula
   url "https://github.com/amiaopensource/vrecord/archive/v2019-07-22.tar.gz"
   version "2019-07-22"
   sha256 "534e9daff42c95e300969e840b278a91ada3db739220bf67bd989834298bd11c"
+  revision 1
 
   bottle :unneeded
 
@@ -18,7 +19,6 @@ class VrecordX < Formula
   depends_on "gnuplot"
   depends_on "mediaconch"
   depends_on "mkvtoolnix"
-  depends_on "mpv"
   depends_on "qcli"
   depends_on "sdl"
   depends_on "xmlstarlet"
@@ -31,8 +31,8 @@ class VrecordX < Formula
       opoo "Sorry, cannot patch the original 'vrecord'. Please login."
     else
       patch do
-        url "https://avpres.net/patch/vrecord_2019-07-22_x_2019-07-27.diff"
-        sha256 "d92eafc2d5be8de038a91b011b463f49c3e70e1cf61f8374c0afcdcda96b1cb1"
+        url "https://avpres.net/patch/vrecord_2019-07-22_x_2019-09-28.diff"
+        sha256 "8087d7f4a33b019524bfff6c22af6d0ef8295d40736c4fcc13cc536695ca00d2"
       end
     end
     bin.install "vrecord"
@@ -48,6 +48,16 @@ class VrecordX < Formula
     bin.install "vtest" if build.with? "vtest"
     man1.install "vrecord.1"
     man1.install "vtest.1" if build.with? "vtest"
+  end
+
+  def caveats; <<~EOS
+    `mpv` has been removed from Homebrew. You may instead use the `mpv` cask:
+
+      brew uninstall mpv
+      brew cask install mpv
+
+    which comes with an embedded FFmpeg 4.0.2 and works.
+  EOS
   end
 
   test do
