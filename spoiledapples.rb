466b37d56e5ee2e7a974d7f8ebe4fa123744078e
Reto Kromer <lists@reto.ch>
2020-01-01 10:42:07 +0100

    spoiledapples 2020-01-01

--- spoiledapples.rb
+++ spoiledapples.rb
@@ -2,7 +2,7 @@ class Spoiledapples < Formula
   desc "Emulation of 6502, 680x0 and PowerPC-based Apple computers"
   homepage "https://avpres.net/Spoiled_Apples/"
   url "https://avpres.net/releases/spoiledapples-2020-01-01.tar.gz"
-  sha256 "779176458460f0046d5a34e5bef31eca31f3b23a78de01f40f436c5bb1b8da0a"
+  sha256 "721dd124ab0b4cd8d37df49052033a7336d0ae79d30cee5cdc2c4e4f7bf19ddc"
 
   bottle :unneeded
 
