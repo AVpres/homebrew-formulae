870c175cc150611060bee6a7ae050e925b01b868
Reto Kromer <lists@reto.ch>
2020-02-01 22:00:12 +0100

    vrecord-x 2020-02-01

--- vrecord-x.rb
+++ vrecord-x.rb
@@ -1,10 +1,9 @@
 class VrecordX < Formula
   desc "Vrecord flavour running on Linux, macOS and Windows"
   homepage "https://github.com/amiaopensource/vrecord"
-  url "https://github.com/amiaopensource/vrecord/archive/v2019-11-14.tar.gz"
-  version "2019-11-14"
-  sha256 "246035ce52a8cc5017bf6f9d43160a9cb5311836f319a4d5e3accaaa18984419"
-  revision 1
+  url "https://github.com/amiaopensource/vrecord/archive/v2020-01-27.tar.gz"
+  version "2020-01-27"
+  sha256 "d1fa8a0d887cdf6a789cc41222b9f0bb6b63a16ad206eb15f10aef5077dc3aa0"
 
   bottle :unneeded
 
@@ -19,6 +18,7 @@ class VrecordX < Formula
   depends_on "gnuplot"
   depends_on "mediaconch"
   depends_on "mkvtoolnix"
+  depends_on "mpv"
   depends_on "qcli"
   depends_on "sdl"
   depends_on "xmlstarlet"
@@ -31,20 +31,20 @@ class VrecordX < Formula
       opoo "Sorry, cannot patch the original 'vrecord'. Please login."
     else
       patch do
-        url "https://avpres.net/patch/vrecord_2019-11-14_x_2020-01-05.diff"
-        sha256 "7d09653a8f60fd8b9090a45b948b06d3ef10d5ae79b3555e020d47ec31bfca1c"
+        url "https://avpres.net/patch/vrecord_2020-01-27_x_2020-02-01.diff"
+        sha256 "02185ab421bb696d2b52399e224c5f4227c2ae2683680b36f265ca9dcbee5794"
       end
     end
     bin.install "vrecord"
     prefix.install "Resources/qcview.lua"
     prefix.install "Resources/vrecord_policy_ffv1.xml"
     prefix.install "Resources/vrecord_policy_uncompressed.xml"
-    prefix.install "Resources/vrecord logo.png"
-    prefix.install "Resources/vrecord logo playback.png"
-    prefix.install "Resources/vrecord logo audio.png"
-    prefix.install "Resources/vrecord logo edit.png"
-    prefix.install "Resources/vrecord logo help.png"
-    prefix.install "Resources/vrecord logo documentation.png"
+    prefix.install "Resources/vrecord_logo.png"
+    prefix.install "Resources/vrecord_logo_playback.png"
+    prefix.install "Resources/vrecord_logo_audio.png"
+    prefix.install "Resources/vrecord_logo_edit.png"
+    prefix.install "Resources/vrecord_logo_help.png"
+    prefix.install "Resources/vrecord_logo_documentation.png"
     bin.install "vtest" if build.with? "vtest"
     man1.install "vrecord.1"
     man1.install "vtest.1" if build.with? "vtest"
