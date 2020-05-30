e97e95e9b28dfe95e06a0a32dcf61cad83642fae
Reto Kromer <lists@reto.ch>
2020-05-30 15:25:07 +0200

    openLTFS 2020-05-31

--- openltfs.rb
+++ openltfs.rb
@@ -1,8 +1,8 @@
 class Openltfs < Formula
   desc "LTFS for both tape drives and DIY tape libraries"
   homepage "https://avpres.net/LTO_LTFS/LTFS/"
-  url "https://avpres.net/releases/openltfs-2020-05-30.tar.gz"
-  sha256 "899cbab61d84a2c54580e771e45685d538f2d02e379eff869a951716c6d06ce7"
+  url "https://avpres.net/releases/openltfs-2020-05-31.tar.gz"
+  sha256 "ee8c8a8bb80b98dbb81111f2d058fa062e85aad9566eb5642fdc8b5546ffa544"
 
   bottle :unneeded
 
@@ -12,8 +12,8 @@ class Openltfs < Formula
         opoo "Please login for full installation."
       else
         patch do
-          url "https://avpres.net/patch/openltfs-2020-05-30.diff"
-          sha256 "8aa383010b38eea14fe7be830371011c3e27c346a8fb46381b7b613019c9214e"
+          url "https://avpres.net/patch/openltfs-2020-05-31.diff"
+          sha256 "02ac7d0f33ae65d3b1a9e955cf58e0868bc8211acee565785bd9ea24423c4b2a"
         end
         system "./configure", "--prefix=#{prefix}"
         system "make", "install"
