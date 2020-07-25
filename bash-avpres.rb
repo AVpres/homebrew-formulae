1014938db30f1e8b5c2023d884cfd72a9d1cca0e
Reto Kromer <lists@reto.ch>
2020-07-25 15:44:03 +0200

    fix typo

--- bash-avpres.rb
+++ bash-avpres.rb
@@ -1,7 +1,7 @@
 class BashAvpres < Formula
   desc "Bash scripts for audiovisual preservation"
   homepage "https://avpres.net/Bash/"
-  url "https://avpres.net/releases/bash_avpres-2020-07-25.tar.gz"
+  url "https://avpres.net/releases/bash-avpres-2020-07-25.tar.gz"
   sha256 "696ac5620ea0adc9f11e74f94e420561edd4b9da7879324ee4bd452f7104304d"
   license "BSD-3-Clause"
 
