be7da03ffaa00828b39ace3744ff15fe3b463ac6
Reto Kromer <lists@reto.ch>
2020-08-15 08:43:37 +0200

    spoiledapples 2020-08-15

--- spoiledapples.rb
+++ spoiledapples.rb
@@ -1,8 +1,8 @@
 class Spoiledapples < Formula
   desc "Emulate 6502, 680x0 and PowerPC-based Apple computers and clones"
   homepage "https://avpres.net/Spoiled_Apples/"
-  url "https://avpres.net/releases/spoiledapples-2020-07-19.tar.gz"
-  sha256 "a3ba7da0ab011fcaf64cbcf2da3a0ee0b1542190f487b7f1a88ffc93468a4a42"
+  url "https://avpres.net/releases/spoiledapples-2020-08-15.tar.gz"
+  sha256 "25e642ca30fa24258b717987004f15d3c0ee128112c1a197e567f9d72cf78a67"
   license "BSD-3-Clause"
 
   option "with-6502", "Enable 6502 assembly language support"
