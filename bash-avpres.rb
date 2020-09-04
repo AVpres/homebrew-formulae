4bf447b3b0d863aa7a8a6ef79e9493af612a463b
Reto Kromer <lists@reto.ch>
2020-09-04 14:59:11 +0200

    bash-avpres: update homepage

--- bash-avpres.rb
+++ bash-avpres.rb
@@ -1,6 +1,6 @@
 class BashAvpres < Formula
   desc "Bash scripts for audiovisual preservation"
-  homepage "https://avpres.net/Bash/"
+  homepage "https://avpres.net/Bash_AVpres/"
   url "https://avpres.net/releases/bash-avpres-2020-09-01.tar.gz"
   sha256 "975c4ea9ffaf2015f0dfe479d996b393bb36a6a26c17b14ac40e00eb27b79ee8"
   license "BSD-3-Clause"
