ec4a65ed82e43636f474a2449434e824653cbb92
Reto Kromer <lists@reto.ch>
2020-09-05 15:11:31 +0200

    openlto 2020-09-05

--- openlto.rb
+++ openlto.rb
@@ -1,8 +1,8 @@
 class Openlto < Formula
   desc "Bash scripts to manage LTO cartridges with LTFS"
   homepage "https://avpres.net/LTO_LTFS/LTO/"
-  url "https://avpres.net/releases/openlto-2020-04-05.tar.gz"
-  sha256 "feb2d79c2070e85574969c8671c93c57baad4351234469ee83b26884d7e99548"
+  url "https://avpres.net/releases/openlto-2020-09-05.tar.gz"
+  sha256 "26e493afad2d440cd2aaadda985081d400bf2781c275f74c9fddfe5a925852b3"
   license "BSD-3-Clause"
 
   def install
@@ -10,8 +10,8 @@ class Openlto < Formula
       opoo "Please login for full installation."
     else
       patch do
-        url "https://avpres.net/patch/openlto-2020-04-05.diff"
-        sha256 "617a094907290294c80dea0dc10c906688b90f01b3b4ded2c08beadba5423ac2"
+        url "https://avpres.net/patch/openlto-2020-09-05.diff"
+        sha256 "713e21768468d724096055754dc1ac51d893cc8f39dde8cd7b051288bce5dc45"
       end
     end
     bin.install Dir["src/*"]
