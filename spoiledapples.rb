94c0fbbdbfbfd8da77430a1c6b9b5adbba3adb36
Reto Kromer <lists@reto.ch>
2020-10-10 17:20:56 +0200

    spoiledapples 2020-10-10

--- spoiledapples.rb
+++ spoiledapples.rb
@@ -1,28 +1,18 @@
 class Spoiledapples < Formula
   desc "Emulate 6502, 680x0 and PowerPC-based Apple computers and clones"
   homepage "https://avpres.net/Spoiled_Apples/"
-  url "https://avpres.net/releases/spoiledapples-2020-09-01.tar.gz"
-  sha256 "eb23443cca52bf6a5268a6e042bbb48ad23480175ebb5cf88cd5c85d74515157"
+  url "https://avpres.net/releases/spoiledapples-2020-10-10.tar.gz"
+  sha256 "b1d0e1e5c13a1588e4fbd506e8f4d5fd1b75d99dbfbd12a434a3d8be0e4df4a9"
   license "BSD-3-Clause"
-
-  option "with-6502", "Enable 6502 assembly language support"
-  option "with-680x0", "Enable 680x0 assembly language support"
-  option "with-ppc", "Enable PowerPC assembly language support"
-  option "with-z80", "Enable Z80 assembly language support"
 
   def install
     if `curl -s https://avpres.net/patch/ | grep -o login` == "login\n"
       opoo "Please login for full installation."
     else
       patch do
-        url "https://avpres.net/patch/spoiledapples-2020-09-01.diff"
-        sha256 "376bd45fe39f3510ed6f6ce332dc336c894ce8dcdfb1429603814c9d63117c7e"
+        url "https://avpres.net/patch/spoiledapples-2020-10-10.diff"
+        sha256 "38f75c3ee64913aa86e9704f392518b1784921220c52719d5c2758367eeb8813"
       end
-
-      args << "--enable-6502" if build.with? "6502"
-      args << "--enable-680x0" if build.with? "680x0"
-      args << "--enable-ppc" if build.with? "ppc"
-      args << "--enable-z80" if build.with? "z80"
 
       system "./configure", *args
       system "make"
