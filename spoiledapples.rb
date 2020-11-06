31a7b64ae40ce5b286749327c0c5d0a9b3102f8f
Reto Kromer <lists@reto.ch>
2020-11-06 15:46:27 +0100

    spoiledapples 2020-11-07

--- spoiledapples.rb
+++ spoiledapples.rb
@@ -1,8 +1,8 @@
 class Spoiledapples < Formula
   desc "Emulate 6502, 680x0 and PowerPC-based Apple computers and clones"
   homepage "https://avpres.net/Spoiled_Apples/"
-  url "https://avpres.net/releases/spoiledapples-2020-10-10.tar.gz"
-  sha256 "b1d0e1e5c13a1588e4fbd506e8f4d5fd1b75d99dbfbd12a434a3d8be0e4df4a9"
+  url "https://avpres.net/releases/spoiledapples-2020-11-07.tar.gz"
+  sha256 "636cac6eb6ef1e5fb14782d555186dc633642283acd191f1325d8ba3a89c3e15"
   license "BSD-3-Clause"
 
   def install
@@ -10,15 +10,13 @@ class Spoiledapples < Formula
       opoo "Please login for full installation."
     else
       patch do
-        url "https://avpres.net/patch/spoiledapples-2020-10-10.diff"
-        sha256 "38f75c3ee64913aa86e9704f392518b1784921220c52719d5c2758367eeb8813"
+        url "https://avpres.net/patch/spoiledapples-2020-11-07.diff"
+        sha256 "77dc00cbffe3acdf661ba98184530015dc9e4e2f856188f2f98ead19181e3499"
       end
-
       system "./configure"
       system "make"
-      system "make", "install"
     end
-    bin.install Dir["src/*"]
+    bin.install Dir["bin/*"]
     man1.install Dir["man/*"]
   end
 
