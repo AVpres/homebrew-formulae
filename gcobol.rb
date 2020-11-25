9b5d3990d971ae810f5cc7d5f9c390d568df4f01
Reto Kromer <lists@reto.ch>
2020-11-25 13:16:17 +0100

    gcobol: revert

--- gcobol.rb
+++ gcobol.rb
@@ -1,8 +1,13 @@
 class Gcobol < Formula
   desc "Implements much of the COBOL 85 and COBOL 2002 standards"
   homepage "https://sourceforge.net/projects/open-cobol/"
-  url "https://kumisystems.dl.sourceforge.net/project/gnucobol/gnucobol/3.1/gnucobol-3.1-rc1.tar.xz"
-  sha256 "c2e41c2ba520681a67c570d7246d25c31f7f55c8a145aaec3f6273a500a93a76"
+  url "https://downloads.sourceforge.net/project/open-cobol/gnu-cobol/2.2/gnucobol-2.2.tar.xz"
+  sha256 "dc18fc45c269debfe86a4bbe20a7250983cba6238ea1917e135df5926cd024a0"
+
+  head do
+    url "https://kumisystems.dl.sourceforge.net/project/gnucobol/gnucobol/3.1/gnucobol-3.1-rc1.tar.xz"
+    sha256 "c2e41c2ba520681a67c570d7246d25c31f7f55c8a145aaec3f6273a500a93a76"
+  end
 
   depends_on "berkeley-db"
   depends_on "gmp"
