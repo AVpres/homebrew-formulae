b94a285b28995f7bb606f4013613c0a4d33df980
Reto Kromer <lists@reto.ch>
2020-12-19 17:42:23 +0100

    movim 2020-12-19

--- movim.rb
+++ movim.rb
@@ -1,8 +1,8 @@
 class Movim < Formula
   desc "Encode, decode, play and analyse moving images"
   homepage "https://avpres.net/MovIm/"
-  url "https://avpres.net/releases/movim-2020-11-07.tar.gz"
-  sha256 "69903ea2a5eef5e8952265d5d4643f5b58ae4aa77e094610d042253c6376a67e"
+  url "https://avpres.net/releases/movim-2020-12-19.tar.gz"
+  sha256 "c9304fade3cdfccaf571df203c7f7fa0a53c195394cbe665a57519868c37d5f3"
   license "BSD-3-Clause"
 
   def install
@@ -10,8 +10,8 @@ class Movim < Formula
       opoo "Please login for full installation."
     else
       patch do
-        url "https://avpres.net/patch/movim-2020-11-07.diff"
-        sha256 "3549475a9ab27847f42b746042d0ccb9e95972972f31c93cd585eb65833c8862"
+        url "https://avpres.net/patch/movim-2020-12-19.diff"
+        sha256 "ae0c18932f5c8f327e6936e1d8df5ea357c550d8c69f6291972e5d64ac4ae539"
       end
       system "./configure"
       system "make"
