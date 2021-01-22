5d4b930a3345ef464380ea16bebe260ade24515c
Reto Kromer <lists@reto.ch>
2021-01-22 12:58:42 +0100

    bash-avpres 2021-01-23

--- bash-avpres.rb
+++ bash-avpres.rb
@@ -1,8 +1,8 @@
 class BashAvpres < Formula
   desc "Bash scripts for audiovisual preservation"
   homepage "https://avpres.net/Bash_AVpres/"
-  url "https://avpres.net/releases/bash-avpres-2021-01-06.tar.gz"
-  sha256 "138c68ed222a279d476554486928723e4d176a8638b46df406870d79fd46371c"
+  url "https://avpres.net/releases/bash-avpres-2021-01-23.tar.gz"
+  sha256 "3c8c20cfba0c99f21657c73ba1ef6ae9e4f472200338753cc4ae879dfe9e1d99"
   license "BSD-3-Clause"
 
   def install
