4d5ca5162d893b0c846fefdc5b32d75d6599788a
Reto Kromer <lists@reto.ch>
2021-01-26 09:09:56 +0100

    add vrecord-x 2021-01-25

---
+++ vrecord-x.rb
@@ -0,0 +1,57 @@
+class VrecordX < Formula
+  desc "Vrecord flavour running on Linux, macOS and Windows"
+  homepage "https://github.com/amiaopensource/vrecord"
+  url "https://github.com/amiaopensource/vrecord/archive/v2021-01-25.tar.gz"
+  version "2021-01-25"
+  sha256 "2e62563e71f98ca4eb33bb2989989c9ccdb8085fe1292eacf4c7efd9e4112589"
+
+  option "with-doc", "Install the documentation"
+  option "with-vtest", "Test streams from computer out to monitor through Blackmagic card"
+
+  depends_on "amiaopensource/amiaos/decklinksdk"
+  depends_on "amiaopensource/amiaos/ffmpegdecklink"
+  depends_on "amiaopensource/amiaos/gtkdialog"
+  depends_on "cowsay"
+  depends_on "gnuplot"
+  depends_on "mediaconch"
+  depends_on "mkvtoolnix"
+  depends_on "mpv"
+  depends_on "qcli"
+  depends_on "sdl"
+  depends_on "xmlstarlet"
+
+  conflicts_with "amiaopensource/amiaos/vrecord",
+    :because => "both install the same resources"
+
+  def install
+    if `curl -s https://avpres.net/patch/ | grep -o login` == "login\n"
+      opoo "Sorry, cannot patch the original 'vrecord'. Please login."
+    else
+      patch do
+        url "https://avpres.net/patch/vrecord_2021-01-25_x_2021-01-26.diff"
+        sha256 "fab6a3f1163b38482fcf73319dbe42308ba77445f41bbd7ddfecf42487547418"
+      end
+    end
+
+    bin.install "vrecord"
+    prefix.install "Resources/dvrecord.png"
+    prefix.install "Resources/qcview.lua"
+    prefix.install "Resources/vrecord_functions"
+    prefix.install "Resources/vrecord_policy_ffv1.xml"
+    prefix.install "Resources/vrecord_policy_uncompressed.xml"
+    prefix.install "Resources/vrecord_logo.png"
+    prefix.install "Resources/vrecord_logo_playback.png"
+    prefix.install "Resources/vrecord_logo_audio.png"
+    prefix.install "Resources/vrecord_logo_edit.png"
+    prefix.install "Resources/vrecord_logo_help.png"
+    prefix.install "Resources/vrecord_logo_documentation.png"
+    bin.install "vtest" if build.with? "vtest"
+    man1.install "vrecord.1"
+    man1.install "vtest.1" if build.with? "vtest"
+  end
+
+  test do
+    system "#{bin}/vrecord", "-h"
+    system "#{bin}/vtest", "-h" if build.with? "vtest"
+  end
+end
