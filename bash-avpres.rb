50a77a304d997c05b8c3cd00105b2cbb9686b19f
Reto Kromer <retokromer@users.noreply.github.com>
2021-03-17 10:36:00 +0100

    bash-avpres 2021-03-17
    - verify_bagit: bug fix

--- bash-avpres.rb
+++ bash-avpres.rb
@@ -1,8 +1,8 @@
 class BashAvpres < Formula
   desc "Bash scripts for audiovisual preservation"
   homepage "https://avpres.net/Bash_AVpres/"
-  url "https://avpres.net/releases/bash-avpres-2021-03-13.tar.gz"
-  sha256 "37d50f472b576d0de82be1e91e2e63724e23b5cea701502228795640cd80bd1f"
+  url "https://avpres.net/releases/bash-avpres-2021-03-17.tar.gz"
+  sha256 "6d19863c5b4466d1104e948da3b5e0c73a9cde4086fa713d96a07b6bae66e8cd"
   license "BSD-3-Clause"
 
   def install
