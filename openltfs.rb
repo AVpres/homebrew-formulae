b6b41d7bfd0a429adf9a7b37241802020d23db04
Reto Kromer <retokromer@users.noreply.github.com>
2021-12-16 13:48:23 +0100

    openltfs 2021-12-18

--- openltfs.rb
+++ openltfs.rb
@@ -1,8 +1,8 @@
 class Openltfs < Formula
   desc "LTFS for both tape drives and DIY tape libraries"
   homepage "https://avpres.net/LTO_LTFS/LTFS/"
-  url "https://avpres.net/releases/openltfs-2021-10-23.tar.gz"
-  sha256 "fd920eec4fd5184707c5aa9b038886ac34eedab8b5d6ab149cfbeee24e9653c2"
+  url "https://avpres.net/releases/openltfs-2021-12-18.tar.gz"
+  sha256 "2f16f5e0bd0741df0aa72d732e8858dfa314c5a4f8e220bed74848d643d9ac76"
   license "BSD-3-Clause"
 
   def install
@@ -10,8 +10,8 @@ class Openltfs < Formula
       opoo "Please login for full installation."
     else
       patch do
-        url "https://avpres.net/patch/openltfs-2021-10-23.diff"
-        sha256 "00a5dd5c6f77d33ac28005638616d02eeaf1ae725435486ebf988453c13768c4"
+        url "https://avpres.net/patch/openltfs-2021-12-18.diff"
+        sha256 "78c9b1a892f5a91d8699aa8e8d7db1e0e59cebea3a5e7863022a63f59293e207"
       end
       system "./configure", "--prefix=#{prefix}"
       system "make", "install"
