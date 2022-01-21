0115df302b6fd83bea3dd3199ed2773245fa9359
Reto Kromer <retokromer@users.noreply.github.com>
2022-01-21 14:33:03 +0100

    openlto 2022-01-22

--- openlto.rb
+++ openlto.rb
@@ -1,8 +1,8 @@
 class Openlto < Formula
   desc "Bash scripts to manage LTO cartridges with LTFS"
   homepage "https://avpres.net/LTO_LTFS/LTO/"
-  url "https://avpres.net/releases/openlto-2021-12-18.tar.gz"
-  sha256 "1513814a4ce88d3fca1188a1ed34dd30efefaeed1df6d60ce022804f24dc9387"
+  url "https://avpres.net/releases/openlto-2022-01-22.tar.gz"
+  sha256 "07755161a97a942b16fbc8c76427a385b876e292fcc316ba9f78138bee61c94b"
   license "BSD-3-Clause"
 
   def install
@@ -10,9 +10,11 @@ class Openlto < Formula
       opoo "Please login for full installation."
     else
       patch do
-        url "https://avpres.net/patch/openlto-2021-12-18.diff"
-        sha256 "b0e4d7d1b90358d49e246fa3c70967ad8df0946db57e3177268babc1456b9a52"
+        url "https://avpres.net/patch/openlto-2022-01-22.diff"
+        sha256 "509273d436f70f7bce6623252506722eb19d8743f5a7224b0822da0072adc287"
       end
+      system "./configure"
+      system "make"
     end
     bin.install Dir["bin/*"]
     man1.install Dir["man/*"]
