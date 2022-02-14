1539dcf95d438b36993b819a1a680b6e17349474
Reto Kromer <retokromer@users.noreply.github.com>
2022-02-14 16:47:40 +0100

    add libewf-old

---
+++ libewf-old.rb
@@ -0,0 +1,27 @@
+class LibewfOld < Formula
+  desc "Library for support of the Expert Witness Compression Format"
+  homepage "https://github.com/libyal/libewf"
+  url "https://github.com/libyal/libewf-legacy/releases/download/20140812/libewf-20140812.tar.gz"
+  sha256 "be90b7af2a63cc3f15d32ce722a19fbd5bbb0173ce20995ba2b27cc9072d6f25"
+  license "LGPL-3.0-or-later"
+
+  depends_on "pkg-config" => :build
+  depends_on "openssl@1.1"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "zlib"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --with-libfuse=yes
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ewfinfo -V")
+  end
+end
