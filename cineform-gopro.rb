b51b3196c5c65a27e7326cadb5e914b7dffd7e9f
Reto Kromer <retokromer@users.noreply.github.com>
2022-02-20 06:18:25 +0100

    cineform-gopro 10.1.1

--- cineform-gopro.rb
+++ cineform-gopro.rb
@@ -1,8 +1,8 @@
 class CineformGopro < Formula
   desc "GoPro's static CineForm library"
   homepage "https://gopro.github.io/cineform-sdk"
-  url "https://github.com/gopro/cineform-sdk/archive/v10.1.0.tar.gz"
-  sha256 "7ce1cf322ef7d9d9cb2de66677b3fa2c5cc07e08f654e1c1381fca41f2a6a788"
+  url "https://github.com/gopro/cineform-sdk/archive/v10.1.1.tar.gz"
+  sha256 "f82ac3e841185329a8a4a7a0752d7342159b71bab1cbf0a4eb0ca32e4a9ed48b"
   head "https://github.com/gopro/cineform-sdk.git"
 
   option "with-TestCFHD", "Install TestCFHD utility"
