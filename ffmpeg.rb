f85ea4a5318030e51ef82418529de73c3622c7d6
Reto Kromer <retokromer@users.noreply.github.com>
2022-03-08 15:12:54 +0100

    ffmpeg: bump revision

--- ffmpeg.rb
+++ ffmpeg.rb
@@ -4,7 +4,7 @@ class Ffmpeg < Formula
   url "https://ffmpeg.org/releases/ffmpeg-5.0.tar.xz"
   sha256 "51e919f7d205062c0fd4fae6243a84850391115104ccf1efc451733bc0ac7298"
   license "GPL-2.0-or-later"
-  revision 1
+  revision 2
   head "https://github.com/FFmpeg/FFmpeg.git"
 
   option "with-chromaprint", "Enable the Chromaprint audio fingerprinting library"
