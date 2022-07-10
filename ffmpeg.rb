39499c9158da15a62cddb461f2b6682d0a3943a1
רטו • Reto <retokromer@users.noreply.github.com>
2022-07-10 20:22:18 +0200

    depends on "alsa-lib" on Linux (#9)

--- ffmpeg.rb
+++ ffmpeg.rb
@@ -4,7 +4,7 @@ class Ffmpeg < Formula
   url "https://ffmpeg.org/releases/ffmpeg-5.0.1.tar.xz"
   sha256 "ef2efae259ce80a240de48ec85ecb062cecca26e4352ffb3fda562c21a93007b"
   license "GPL-2.0-or-later"
-  revision 4
+  revision 5
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
 
   option "with-chromaprint", "Enable the Chromaprint audio fingerprinting library"
@@ -92,6 +92,7 @@ class Ffmpeg < Formula
   uses_from_macos "zlib"
 
   on_linux do
+    depends_on "alsa-lib"
     depends_on "libxv"
     depends_on "gcc" => :optional
   end
