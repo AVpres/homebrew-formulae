7cbfc872cadbfca4081c40797934736e2afddcf4
Reto Kromer <retokromer@users.noreply.github.com>
2022-10-08 21:04:50 +0200

    MovIm 2022-10-08

--- movim.rb
+++ movim.rb
@@ -1,8 +1,8 @@
 class Movim < Formula
   desc "Encode, decode, play and analyse moving images"
   homepage "https://avpres.net/MovIm/"
-  url "https://avpres.net/releases/movim-2022-08-13.tar.gz"
-  sha256 "9c9b4a27f420457953cc6f4027fbdb989b2345ae5145d05918f5898a9c438597"
+  url "https://avpres.net/releases/movim-2022-10-08.tar.gz"
+  sha256 "cb693c346caff06fc84db21bb503d618233b8624865f363a94fc8df03931e5f1"
   license "BSD-3-Clause"
 
   def install
@@ -10,8 +10,8 @@ class Movim < Formula
       opoo "Please login for full installation."
     else
       patch do
-        url "https://avpres.net/patch/movim-2022-08-13.diff"
-        sha256 "5a31b26f4538ebad50d95dd1379a6a5854c55543f656a9eef97689fb3acd6e65"
+        url "https://avpres.net/patch/movim-2022-10-08.diff"
+        sha256 "042c22d5b89ddc0141108916ac34331668b6dcb2bc848a8520385c9a0bb0a680"
       end
       system "./configure"
       system "make"
