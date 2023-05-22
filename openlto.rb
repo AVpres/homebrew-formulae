d432f0c1464f863e48815eba910b1101c2b11314
Reto Kromer <retokromer@users.noreply.github.com>
2023-05-22 20:37:01 +0200

    openLTO 2023-05-23

--- openlto.rb
+++ openlto.rb
@@ -5,8 +5,8 @@
 class Openlto < Formula
   desc "Bash scripts to manage LTO cartridges with LTFS"
   homepage "https://avpres.net/LTO_LTFS/LTO/"
-  url "https://avpres.net/releases/openlto-2022-12-03.tar.gz"
-  sha256 "c9d9d8f8883b28e2f5083741c44d869b2403babe4b1e6840957fe8e33a5622c3"
+  url "https://avpres.net/releases/openlto-2023-05-20.tar.gz"
+  sha256 "615283e69732d47047a4315cbaf5475ab9ea008a317bd929bb7e7a72ca7046f3"
   license "BSD-3-Clause"
 
   def install
@@ -14,8 +14,8 @@ class Openlto < Formula
       opoo "Please login for full installation."
     else
       patch do
-        url "https://avpres.net/patch/openlto-2022-12-03.diff"
-        sha256 "6f88df58c3076034ea4736de2be8319262724bbdeeab9e0a47ffa3d480c2cab0"
+        url "https://avpres.net/patch/openlto-2023-05-20.diff"
+        sha256 "6655673de8731be2a9c80ae83008e964445875b693a8a3595dccea7840299a46"
       end
       system "./configure"
       system "make"
