62a7e51ae6efc79f3a90dc2c5151db5b726a3846
רטו/רעטאָ/רֵיטוֹ • Reto <retokromer@users.noreply.github.com>
2023-11-10 06:31:26 +0100

    vrecord-x: use FFmpeg 6.0.1

--- vrecord-x.rb
+++ vrecord-x.rb
@@ -7,7 +7,7 @@ class VrecordX < Formula
   homepage "https://github.com/amiaopensource/vrecord"
   url "https://github.com/amiaopensource/vrecord/archive/refs/tags/v2023-08-07.tar.gz"
   sha256 "2572ea969a42de32499dc74c663878f73c2de34e2d3022c409008f057321d032"
-  revision 1
+  revision 2
 
 
   option "with-doc", "Install the documentation locally"
@@ -34,8 +34,8 @@ class VrecordX < Formula
       opoo "Sorry, cannot patch the original 'vrecord'. Please login."
     else
       patch do
-        url "https://avpres.net/patch/vrecord_2023-08-07_x_2023-10-04.diff"
-        sha256 "48786a69a2addb032df2383bc228e664efea0681146c19bc9ae9a8272faf33d6"
+        url "https://avpres.net/patch/vrecord_2023-08-07_x_2023-11-11.diff"
+        sha256 "23de8adaf9dcf67c218ae0f51c774a9571e7f6e16084fcd7d92171d0430e258d"
       end
       args << "--with-doc" if build.with? "doc"
       args << "--with-vtest" if build.with? "vtest"
