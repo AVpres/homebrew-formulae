65c7e517a076c8b0b06d2a266496b4f5fcf66f42
Reto Kromer <retokromer@users.noreply.github.com>
2023-12-29 15:01:00 +0100

    Spoiled_Apples 2023-12-30

--- spoiledapples.rb
+++ spoiledapples.rb
@@ -5,8 +5,8 @@
 class Spoiledapples < Formula
   desc "Emulate 6502, 680x0 and PowerPC-based Apple computers and clones"
   homepage "https://avpres.net/Spoiled_Apples/"
-  url "https://avpres.net/releases/spoiledapples-2023-08-19.tar.gz"
-  sha256 "c8d626eef345b6c014d67ff98203cbc931632eba836344666cd5f1ef9b2d1799"
+  url "https://avpres.net/releases/spoiledapples-2023-12-30.tar.gz"
+  sha256 "f03c20aa507f485397066e61c63bf31c02d60c59dd1ad119d81cbda7c00a581d"
   license "BSD-3-Clause"
 
   def install
@@ -14,8 +14,8 @@ class Spoiledapples < Formula
       opoo "Please login for full installation."
     else
       patch do
-        url "https://avpres.net/patch/spoiledapples-2023-08-19.diff"
-        sha256 "4dc79747c5f9eac7b4d7471db3461a30a4f793b40be6a8755d64387eacb0ce8a"
+        url "https://avpres.net/patch/spoiledapples-2023-12-30.diff"
+        sha256 "ae8b5744543e65366187387f34e5ddf493a000b76139924d5ca32e9feb55ffdc"
       end
       system "./configure"
       system "make"
