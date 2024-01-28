406f318feef91d7ae7b86b251ab8464e10f9dbb3
Reto Kromer <retokromer@users.noreply.github.com>
2024-01-28 13:23:05 +0100

    Bash_AVpres 2024-01-27
    update year regex

--- bash-avpres.rb
+++ bash-avpres.rb
@@ -5,8 +5,8 @@
 class BashAvpres < Formula
   desc "Bash scripts for audiovisual preservation"
   homepage "https://avpres.net/Bash_AVpres/"
-  url "https://avpres.net/releases/bash-avpres-2023-09-09.tar.gz"
-  sha256 "619e723afa7e6efb234101470199f56af3463e741743b36187b01ea7f1158b27"
+  url "https://avpres.net/releases/bash-avpres-2024-01-27.tar.gz"
+  sha256 "b4a7cdd2c403f9c91dfa956121287d59ba1197625914bdc6a235e85658cdab32"
   license "BSD-3-Clause"
 
   def install
