6848d23a5e989c5ed850c1f3e155e5a713afe75e
Reto Kromer <retokromer@users.noreply.github.com>
2024-05-26 06:39:10 +0200

    FFmpeg 7.0.1

--- ffmpeg.rb
+++ ffmpeg.rb
@@ -5,10 +5,9 @@
 class Ffmpeg < Formula
   desc "Alternate FFmpeg formula with options"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-7.0.tar.gz"
-  sha256 "943a2a28044947c17a905c39075494b0da46ec0795224c2c61eff986518321eb"
+  url "https://ffmpeg.org/releases/ffmpeg-7.0.1.tar.gz"
+  sha256 "bce9eeb0f17ef8982390b1f37711a61b4290dc8c2a0c1a37b5857e85bfb0e4ff"
   license "GPL-2.0-or-later"
-  revision 2
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
 
   option "with-aribb24", "Enable ARIB STD-B24, decoding JIS 8 bit characters and parsing MPEG-TS"
