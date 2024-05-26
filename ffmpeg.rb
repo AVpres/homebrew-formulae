b063b4de60050fc8c9cccb98bde7242a4368050a
Reto Kromer <retokromer@users.noreply.github.com>
2024-05-26 06:42:08 +0200

    switch back to xz

--- ffmpeg.rb
+++ ffmpeg.rb
@@ -5,7 +5,7 @@
 class Ffmpeg < Formula
   desc "Alternate FFmpeg formula with options"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-7.0.1.tar.gz"
+  url "https://ffmpeg.org/releases/ffmpeg-7.0.1.tar.xz"
   sha256 "bce9eeb0f17ef8982390b1f37711a61b4290dc8c2a0c1a37b5857e85bfb0e4ff"
   license "GPL-2.0-or-later"
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
