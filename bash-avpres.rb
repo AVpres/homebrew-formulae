72775783d7bdd44b0036047b713d6ba1430edc8f
Reto Kromer <retokromer@users.noreply.github.com>
2025-04-21 15:10:42 +0200

    Bash_AVpres: add dependencies explicitly

--- bash-avpres.rb
+++ bash-avpres.rb
@@ -1,7 +1,3 @@
-# typed: true
-# frozen_string_literal: true
-
-# Bash scripts for audiovisual preservation
 class BashAvpres < Formula
   desc "Bash scripts for audiovisual preservation"
   homepage "https://avpres.net/Bash_AVpres/"
@@ -9,12 +5,16 @@ class BashAvpres < Formula
   sha256 "063c8da5533bfb33021a34cb5f27cb229e7de5eedaf730fc4e6d8999b3dcb270"
   license "BSD-3-Clause"
 
+  depends_on "bash"
+  depends_on "coreutils"
+  depends_on "xxhash"
+
   def install
     bin.install Dir["bin/*"]
     man1.install Dir["man/*"]
   end
 
   test do
-    system "#{bin}/bash_avpres"
+    system bin/"bash_avpres"
   end
 end
