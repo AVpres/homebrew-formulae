a68e2134d156a55307fc3067ee496fbbcdfb52f7
Reto Kromer <retokromer@users.noreply.github.com>
2025-04-29 07:12:53 +0200

    vrecord-x: 2025-04-21

--- vrecord-x.rb
+++ vrecord-x.rb
@@ -1,8 +1,8 @@
 class VrecordX < Formula
   desc "Vrecord flavour running on Linux, macOS and Windows"
   homepage "https://github.com/amiaopensource/vrecord"
-  url "https://github.com/amiaopensource/vrecord/archive/refs/tags/v2024-10-18.tar.gz"
-  sha256 "f2c0a3bc9ad36686f07d8fba7364f296be9253aac7e6dfcad0320b85da628517"
+  url "https://github.com/amiaopensource/vrecord/archive/refs/tags/vrecord_v2025-04-21.tar.gz"
+  sha256 "019d24ddd4b2d5bb0d467d27d42382fa689af7ab3f65fbfe835fa11d84aead33"
 
   option "with-doc", "Install the documentation locally"
   option "with-vtest", "Test streams from computer out to monitor through Blackmagic card"
@@ -28,8 +28,8 @@ class VrecordX < Formula
       opoo "Sorry, cannot patch the original 'vrecord'. Please login."
     else
       patch do
-        url "https://avpres.net/patch/vrecord_2024-10-18_x_2024-10-26.diff"
-        sha256 "8112172fdfa8bdd7243cabd113ab89b72bedb520fd7c34802d94453b62a40b5f"
+        url "https://avpres.net/patch/vrecord_2025-04-21_x_2025-04-29.diff"
+        sha256 "f5c3b7efd91af57f40ceeb82a1ac07f00777e0dd2a34207259f6f1d2163f4a60"
       end
       args << "--with-doc" if build.with? "doc"
       args << "--with-vtest" if build.with? "vtest"
