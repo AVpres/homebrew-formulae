2732300d77f0e2c6d1fdb62a78cb02d5995c577f
Reto Kromer <retokromer@users.noreply.github.com>
2025-10-04 16:22:42 +0200

    test DCP-o-matic

---
+++ dcp-o-matic.rb
@@ -0,0 +1,18 @@
+cask "dcp-o-matic" do
+  version "2.18.25"
+  sha256 "25078a66e629daec57b118a10eec77d03c8aab0d7df30743cdad4a47db734fed"
+
+  url "https://dcpomatic.com/dl.php?id=osx-10.10-main&version=#{version}"
+  name "DCP-o-matic"
+  desc "Convert video, audio and subtitles into DCP (Digital Cinema Package)"
+  homepage "https://dcpomatic.com/"
+
+  livecheck do
+    url "https://dcpomatic.com/download"
+    regex(/stable\s*release:\s*(\d+(?:\.\d+)+)/i)
+  end
+
+  app "DCP-o-matic #{version.major}.app"
+
+  # No zap stanza required
+end
