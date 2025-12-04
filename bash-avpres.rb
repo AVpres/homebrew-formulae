6898b8ba4cf4b50cefc52eae3077bbce7272238b
רטו\רעטאָ\רֵיטוֹ • Reto <retokromer@users.noreply.github.com>
2025-12-04 13:45:44 +0100

    Bash_AVpres 2025-12-04

--- bash-avpres.rb
+++ bash-avpres.rb
@@ -1,8 +1,8 @@
 class BashAvpres < Formula
   desc "Bash scripts for audio-visual preservation"
   homepage "https://avpres.net/Bash_AVpres/"
-  url "https://avpres.net/releases/bash-avpres-2025-12-02.tar.gz"
-  sha256 "f4bd52e879824f47aff9d6edbb27312e3b217451de783ecedf2d9f3b314472eb"
+  url "https://avpres.net/releases/bash-avpres-2025-12-04.tar.gz"
+  sha256 "e088916093f8ed21e7d9c17a56b5ba34579f3fa9888c76a611d7e03d733fbf5d"
   license "BSD-3-Clause"
 
   depends_on "bash"
@@ -15,7 +15,7 @@ class BashAvpres < Formula
   end
 
   def post_install
-    system "bash_avpres"
+    "bash_avpres"
   end
 
   test do
