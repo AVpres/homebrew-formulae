aa39e08405a76f87caa23c48eac0df8d9a2a72c6
רטו\רעטאָ\רֵיטוֹ • Reto <retokromer@users.noreply.github.com>
2026-01-25 07:59:49 +0100

    update FFmpeg

--- ffmpeg.rb
+++ ffmpeg.rb
@@ -71,7 +71,6 @@ class Ffmpeg < Formula
   depends_on "theora" => :recommended
   depends_on "x264" => :recommended
   depends_on "x265" => :recommended
-  depends_on "xz" => :recommended
 
   depends_on "aribb24" => :optional
   depends_on "chromaprint" => :optional
@@ -133,6 +132,7 @@ class Ffmpeg < Formula
     depends_on "libdrm"
     depends_on "libxext"
     depends_on "libxv"
+    depends_on "xz"
     depends_on "libx11" => :recommended
     depends_on "libxcb" => :recommended
   end
