04a23858fa2afcd1d89c665ce30c8f8763fbbcf4
רטו\רעטאָ\רֵיטוֹ • Reto <retokromer@users.noreply.github.com>
2026-05-17 08:01:52 +0200

    FFmpeg 2026-05-18

--- bash-avpres.rb
+++ bash-avpres.rb
@@ -1,8 +1,8 @@
 class BashAvpres < Formula
-  desc "Bash scripts for audio-visual preservation"
+  desc "Bash scripts for audiovisual preservation"
   homepage "https://avpres.net/Bash_AVpres/"
-  url "https://avpres.net/releases/bash-avpres-2026-03-08.tar.gz"
-  sha256 "03a1392aa3c4a23644d75e5869a690590fc7dc36fd07948577bba3f711fc84fe"
+  url "https://avpres.net/releases/bash-avpres-2026-05-18.tar.gz"
+  sha256 "7fcba6b6a06d6f18b41f819ae4ce6950bf23247ac79ee283cb1a5e31660ef066"
   license "BSD-3-Clause"
 
   depends_on "bash"
@@ -12,6 +12,15 @@ class BashAvpres < Formula
   def install
     bin.install Dir["bin/*"]
     man1.install Dir["man/*"]
+  end
+
+  def caveats
+    on_linux do
+      <<~EOS
+        If you wish to use the CRC32 algorithm, you may need to install a CRC32 tool:
+          sudo apt install libarchive-zip-perl
+      EOS
+    end
   end
 
   test do
