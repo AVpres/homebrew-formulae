385ca515685aba6e02eb764bc893997586c16cdd
רטו\רעטאָ\רֵיטוֹ • Reto <retokromer@users.noreply.github.com>
2026-06-07 09:59:15 +0200

    mpv: align to official formula

--- mpv.rb
+++ mpv.rb
@@ -3,8 +3,8 @@ class Mpv < Formula
   homepage "https://mpv.io"
   url "https://github.com/mpv-player/mpv/archive/refs/tags/v0.41.0.tar.gz"
   sha256 "ee21092a5ee427353392360929dc64645c54479aefdb5babc5cfbb5fad626209"
-  license :cannot_represent
-  head "https://github.com/mpv-player/mpv.git", branch: "master"
+  license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
+  revision 1
 
   depends_on "docutils" => :build
   depends_on "meson" => :build
@@ -50,8 +50,15 @@ class Mpv < Formula
     depends_on "zlib-ng-compat"
   end
 
+  # Backport support for Vapoursynth 74+
+  patch do
+    url "https://github.com/mpv-player/mpv/commit/75b2ccfeb1ce4ed5a40ac9860fa74f3d1265e13f.patch?full_index=1"
+    sha256 "3906b98b02071a0d5747a400406494ca69cef7afd8d3eee4a99fdbe40dc90c1f"
+  end
+
   def install
     args = %W[
+      --sysconfdir=#{etc}
       -Dbuild-date=false
       -Dhtml-build=enabled
       -Djavascript=enabled
@@ -60,9 +67,6 @@ class Mpv < Formula
       -Dlibarchive=enabled
       -Duchardet=enabled
       -Dvulkan=enabled
-      --sysconfdir=#{pkgetc}
-      --datadir=#{pkgshare}
-      --mandir=#{man}
     ]
 
     if OS.linux?
@@ -76,6 +80,7 @@ class Mpv < Formula
     system "meson", "setup", "build", *args, *std_meson_args
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
+    bash_completion.install share/"bash-completion/completions/mpv"
 
     if OS.mac?
       # `pkg-config --libs mpv` includes libarchive, but that package is
@@ -86,9 +91,6 @@ class Mpv < Formula
                 /^Requires\.private:(.*)\blibarchive\b(.*?)(,.*)?$/,
                 "Requires.private:\\1#{libarchive}/lib/pkgconfig/libarchive.pc\\3"
     end
-
-    bash_completion.install "etc/mpv.bash-completion" => "mpv"
-    zsh_completion.install "etc/_mpv.zsh" => "_mpv"
   end
 
   test do
