0b2a957d635b60a7a4d0a8e8bf79ec26a77d46d6
רטו\רעטאָ\רֵיטוֹ • Reto <retokromer@users.noreply.github.com>
2026-08-04 10:21:59 +0200

    FFmpeg 9.0

--- ffmpeg.rb
+++ ffmpeg.rb
@@ -1,10 +1,9 @@
 class Ffmpeg < Formula
   desc "Alternative FFmpeg formula with options"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-8.1.2.tar.xz"
-  sha256 "464beb5e7bf0c311e68b45ae2f04e9cc2af88851abb4082231742a74d97b524c"
+  url "https://ffmpeg.org/releases/ffmpeg-9.0.tar.xz"
+  sha256 "7f607a00dd0d28a729d5a4811205812eef01cf6ef6155025febb6f36a9062d52"
   license "GPL-2.0-or-later"
-  revision 2
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
 
   option "with-aribb24", "Enable ARIB STD-B24, decoding JIS 8 bit characters and parsing MPEG-TS"
